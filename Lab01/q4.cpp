5736ceb91590a03fd375050965e4d53f8f7c3fd7
fatima068 <113721501+fatima068@users.noreply.github.com>
2025-01-22 19:46:28 +0500

    Update q4.cpp

--- Lab01/q4.cpp
+++ Lab01/q4.cpp
@@ -1,4 +1,4 @@
-/*Q4: You are given an integer array height of length n. There are n vertical lines
+/* You are given an integer array height of length n. There are n vertical lines
 drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
 Find two lines that together with the x-axis form a container, such that the
 container contains the most water. Return the maximum amount of water a
@@ -10,8 +10,30 @@ Explanation: The above vertical lines are represented by array
 container can contain is 49.*/
 
 #include <iostream>
-using namespace std; 
+#include <algorithm>
 
-int main (){
+using namespace std;
+
+int main()
+{
+    float area;
+    int i, j, maxArea = 0; 
+    int height[10]; //assuming size of array is always 10
+
+    for (int i=0; i<10; i++)
+    {
+        cout << "Enter your array element: ";
+        cin >> height[i];
+    }
     
+    for (int i = 0; i < 10; i++) 
+    {
+        for (int j = i + 1; j < 10; j++) 
+        {
+            int area = min(height[i], height[j]) * (j - i);
+            maxArea = max(maxArea, area);
+        }
+    }
+    
+    cout << "Maximum area is " << maxArea;
 }
